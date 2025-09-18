32873587cd845da181bc5ab242547203bb49a3a7
robnch30 <robnch30@gmail.com>
2025-09-17 18:45:01 -0600

    Ejercicios destructurin classes

--- Basic/21-destructuring-spreading-exercises.js
+++ Basic/21-destructuring-spreading-exercises.js
@@ -4,22 +4,92 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=16802
 */
 
 // 1. Usa desestructuración para extraer los dos primeros elementos de un array 
+let animals = ['dog','cat','fish','parrot','hamster']
+
+let product ={
+    description: 'coca cola',
+    sellPrice: 20.0,
+    buyPrice: 16.50,
+    stock: 100
+}
+
+let [element0,element1,,,] = animals
+
+console.log(element0)
+console.log(element1)
+
 
 // 2. Usa desestructuración en un array y asigna un valor predeterminado a una variable
 
+let [array0 = '',array1 = '', array2 = '', array3 = '', array4 = ''] = animals
+console.log(array0)
+console.log(array1)
+console.log(array2)
+console.log(array3)
+console.log(array4)
+
 // 3. Usa desestructuración para extraer dos propiedades de un objeto
+
+let stock = product.stock
+let description = product.description
+console.log(stock)
 
 // 4. Usa desestructuración para extraer dos propiedades de un objeto y asígnalas
 //    a nuevas variables con nombres diferentes
 
+let sel = product.sellPrice
+let buy = product.buyPrice
+
+console.log(sel)
+console.log(buy)
+
+
+
 // 5. Usa desestructuración para extraer dos propiedades de un objeto anidado
+
+let car3 = {
+
+    brand: 'Honda',
+    model: 'Civic',
+    year: 1999,
+    makeCountry: 'Mexico',
+    originCountry: 'Japan',
+    color: "black",
+    race: {
+        competition: 'F1',
+        position: 1,
+        receCountrys:['Mexico', 'EUA', 'Monaco']
+    }
+
+}
+
+let competition = car3.race.competition
+let racing = car3.race.receCountrys
+
+console.log(competition, racing)
 
 // 6. Usa propagación para combinar dos arrays en uno nuevo
 
+let myArray = [1, 2, 3, 4]
+
+let newArray = [...myArray, ...animals]
+console.log(newArray)
+
 // 7. Usa propagación para crear una copia de un array
+let copyArray = [...myArray]
+console.log(copyArray)
 
 // 8. Usa propagación para combinar dos objetos en uno nuevo
 
+let newObjet = {...car3, ...product}
+console.log(newObjet)
 // 9. Usa propagación para crear una copia de un objeto
 
+let copyObjet = {...product}
+console.log(copyObjet)
 // 10. Combina desestructuración y propagación
+
+let brand = car3.brand
+
+let destrucPropagation = {...product, brand}
+console.log(destrucPropagation)
--- Basic/23-classes-exercises.js
+++ Basic/23-classes-exercises.js
@@ -4,21 +4,182 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=18630
 */
 
 // 1. Crea una clase que reciba dos propiedades
+class Product{
+    constructor(code, description, stock){
+        this.code = code
+        this.description = description
+        this.stock = stock
+    }
+}
 
 // 2. Añade un método a la clase que utilice las propiedades
+class ProductWhitMetod{
+
+    constructor(code, description, stock){
+        this.code = code
+        this.description = description
+        this.stock = stock
+    }
+
+    validateStock(num){
+        let newStock
+        if(num <= this.stock){
+            console.log('Se puede vender.')
+            newStock = this.stock - num
+            console.log(`Se vendieron ${num} del stok en tienda ${this.stock}
+                Numero nuevo de stock ${newStock}`)
+         }
+
+         return newStock
+    }
+
+}
 
 // 3. Muestra los valores de las propiedades e invoca a la función
 
+let producto3 = new ProductWhitMetod('1234', 'coca cola', 100)
+console.log(producto3.code)
+console.log(producto3.description)
+console.log(producto3.stock)
+
+console.log(producto3.validateStock(3))
+
+
 // 4. Añade un método estático a la primera clase
+
+class ProductWhitStatic{
+    constructor(code, description, stock){
+        this.code = code
+        this.description = description
+        this.stock = stock
+    }
+
+    static subTotal(quantity, salePrice){
+        let subTotal = 0.0
+        subTotal = quantity * salePrice
+        return subTotal
+    }
+}
+
 
 // 5. Haz uso del método estático
 
+console.log(ProductWhitStatic.subTotal(24, 20))
+
 // 6. Crea una clase que haga uso de herencia
+
+class Api{
+    constructor(dominio, url){
+        this.dominio = dominio
+        this.url = url
+    }
+
+    conection(){
+        console.log('Conectando al API')
+    }
+
+    test(){
+        console.log('Test conection Appi ...')
+    }
+}
+
+class mapsApi extends Api {
+    constructor (dominio, url, nameApi, data){
+        super(dominio,url)
+        this.nameApi = nameApi
+        this.data = data
+    }
+
+    getData(){
+        console.log('Datos de la consulta')
+    }
+
+    test(){
+        console.log(`Test ${this.nameApi}`)
+    }
+}
+
+let googleMaps = new mapsApi('https://google.com.mx','/maps', 'googleMaps') 
+
+googleMaps.conection()
+googleMaps.getData()
+
 
 // 7. Crea una clase que haga uso de getters y setters
 
+class ApiGetSet{
+    constructor(dominio, url){
+        this.dominio = dominio
+        this.url = url
+    }
+
+    get getDominio(){
+        return this.dominio
+    }
+
+    set setDominio(dominio){
+        this.dominio = dominio
+    }
+
+    get getUrl(){
+        return this.url
+    }
+
+    set setUrl(url){
+        this.url = url
+    }
+
+    conection(){
+        console.log('Conectando al API')
+    }
+
+
+}
+
+let conetApi = new ApiGetSet('https//facebook.com','/marketPlace')
+console.log(conetApi.getDominio)
+conetApi.setDominio = 'https//facebook.pre.com'
+console.log(conetApi.getDominio)
+
+
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
+
+class Auto {
+    #brand
+    #model
+
+    constructor(brand, model, year){
+        this.#brand = brand
+        this.#model = model
+        this.year = year
+    }
+
+    get getBrand(){
+        return this.brand
+    }
+
+    set setBrand(brand){
+        this.brand = brand
+    }
+
+    get getModel(){
+        return this.model
+    }
+
+    set setModel(model){
+        this.model = model
+    }
+}
 
 // 9. Utiliza los get y set y muestra sus valores
 
-// 10. Sobrescribe un método de una clase que utilice herencia 
+let honda = new Auto('Honda', 'Civic', '1992')
+console.log(honda.year)
+console.log(honda.getBrand)
+honda.setBrand = 'VW'
+console.log(honda.getBrand)
+console.log(honda.getModel)
+
+// 10. Sobrescribe un método de una clase que utilice herencia
+
+googleMaps.test()
